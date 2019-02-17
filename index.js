929aa39772ca21dd9edc28b68b5ca4976e63652a
Mohammed Rashid <rashis@gmail.com>
2019-02-17 13:02:59 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+var express=require('express');
+var socket=require('socket.io');
+var app=express();
+var server =app.listen(4000,function() {
+   console.log("Server is Listening in 4000") 
+});
+
+app.use(express.static('public'));
+
+var io=socket(server);
+io.on('connection',function(socket){
+    console.log('Connected with',socket.id);
+    socket.on('myMsg',function(data){
+        io.sockets.emit('myMsg',data);
+        console.log('Emiited to the server',data);
+    });
+});
---
+++ public/chat.js
@@ -0,0 +1,29 @@
+
+var socket=io.connect('http://localhost:4000');
+
+var output=document.getElementById('output'),
+    sendbtn=document.getElementById('sendbtn'),
+    msg=document.getElementById('smscont'),
+    clrchat=document.getElementById('clrchat'),
+    user=document.getElementById('user');
+    var userin=prompt('Enter Your Name:');
+    user.innerHTML=userin;
+    output.innerHTML+='<p>Welcome <strong>'+ userin+'</strong></p>';
+sendbtn.addEventListener('click',function(){
+    console.log('clicked');
+    socket.emit('myMsg',{
+        message:msg.value,
+        user:userin
+    });
+    msg.value="";
+});
+
+//Listening for event
+
+socket.on('myMsg',function(data){
+    console.log('Recieved Message');
+    output.innerHTML+='<p>'+data.user+':'+ data.message+'</p>';
+});
+clrchat.addEventListener('click',function(){
+    output.innerHTML='';
+});
