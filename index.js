7fb68e790119dcc08427e94148414268d029cee3
Rashidtvmr <44792518+Rashidtvmr@users.noreply.github.com>
2019-02-17 13:30:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var express=require('express');
 var socket=require('socket.io');
 var app=express();
 var server =app.listen(4000,function() {
-   console.log("Server is Listening in 4000") 
+   //console.log("Server is Listening in 4000") 
 });
 
 app.use(express.static('public'));
 
 var io=socket(server);
 io.on('connection',function(socket){
-    console.log('Connected with',socket.id);
+   // console.log('Connected with',socket.id);
     socket.on('myMsg',function(data){
         io.sockets.emit('myMsg',data);
-        console.log('Emiited to the server',data);
+     //   console.log('Emiited to the server',data);
     });
 });
